7c36b5943c0eea0b3fd1fdb7d1e12e79ef41d73d
KI-Cortana <nici-jb91@web.de>
2014-12-16 17:28:45 +0100

    Update applicationApi.js

--- server/applicationApi.js
+++ server/applicationApi.js
@@ -7,12 +7,12 @@ Meteor.methods({
 	
 		var apiKey = 'x';	//individual API key
 		var app = "test"//itemID;									//for APP INFO API: package ID of the requested application, which is the same like the itemID of the results of the SEARCH API
-		var category = 'apps';								//for now we only use the category "apps", but there are the following categories for SEARCH API: "apps", "books", "movies" or "music"
+		var appCategory = 'apps';								//for now we only use the category "apps", but there are the following categories for SEARCH API: "apps", "books", "movies" or "music"
 		//var keyword = searchfield;						//for SEARCH API
 
 		var urlAppInfo = 'http://api.playstoreapi.com/v1.1/apps/' + app + '?key=' + apiKey;
-		var urlSearch = 'http://api.playstoreapi.com/v1.1/search/' + category + '/' + keyword + '?key=' + apiKey;
-	
+		var urlSearch = 'http://api.playstoreapi.com/v1.1/search/' + appCategory + '/' + keyword + '?key=' + apiKey;
+		/*
 		HTTP.get(urlSearch, {params: {}}, function(error,appDetails){
 			if(!error){
 				console.log(appDetails.data);
@@ -27,19 +27,6 @@ Meteor.methods({
 				}
 			}
 		});
-		
-		/*$.getJSON(urlSearch).done(function(appDetails) {
-			for(var i=0; i<appDetails.length; i++){
-					alert(appDetails[i].itemID);
-			}
-			
-			
-			//$('p:last').html(JSON.stringify(appDetails));
-		});
 		*/
-
-		//itemID, welches in den appDetails steht, ist der Name des packages, welcher dann für die Suche nach AppInfo durch urlAppInfo gebraucht wird. 
-		//Es muss (itemID = packageID) geprüft werden, demnach wird dann nach dieser ID gesucht um die benötigten Informationen zu erhalten.
-
 	}
 });
