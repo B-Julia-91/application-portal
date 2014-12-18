e498d83b67b0579b1fb31b39742b078a57889fb8
KI-Cortana <nici-jb91@web.de>
2014-12-19 00:14:07 +0100

    Update application-portal.js

--- client/application-portal.js
+++ client/application-portal.js
@@ -26,8 +26,6 @@ Meteor.startup(function() {
 	Session.set("lying", false);
 	Session.set("horizontal", false);
 	Session.set("vertical", false);
-	Session.set("editID", false);
-	Session.set("motionClicked", false)
 });
 
 //Search field
@@ -50,7 +48,7 @@ Template.searchField.events({
 //Application container
 //Helper 
 Template.appList.helpers({
-    app: function(){ 
+    app: function(){
 		filter = {	};
 		if(Session.get("motionSensors")){
 			filter.motionSensors= true
@@ -59,7 +57,7 @@ Template.appList.helpers({
 			filter.touchscreen= true
 		}
 		if(Session.get("microphone")){
-			filter.mircophone= true
+			filter.microphone= true
 		}
 		if(Session.get("proximitySensor")){
 			filter.proximitySensor= true
@@ -121,9 +119,7 @@ Template.appList.helpers({
 		if(Session.get("vertical")){
 			filter.vertical= true
 		}
-
 		return AppCollection.find(filter)
-
 	},
 	editID: function(){
 		return Session.get("editID");
@@ -133,26 +129,95 @@ Template.appList.helpers({
 Template.appList.events({
     "click #appEdit": function(){
 		var appId = this._id;
+		console.log("Edit pressed")
 		Session.set("selectedApp", appId);
-		Session.set("editPressed", true);
     },
     "click #appSave": function(){
-		return Session.set("savePressed", true);
+		return Session.set("selectedApp", false);
 	}
 });
 
 //App details
 //Helper 
 Template.appDetails.helpers({
+	
     editID: function(){
 		var appId = this._id;
 		var selectedApp = Session.get("selectedApp");
 		if(appId == selectedApp){
+			console.log("Aufruf der editID")
 			return true
 		}
+		else return false
 	},
 	motionClicked: function(){
 		return Session.get("motionClicked")
+	},
+	touchClicked: function(){
+		return Session.get("touchClicked")
+	},
+	micClicked: function(){
+		return Session.get("micClicked")
+	},
+	proxClicked: function(){
+		return Session.get("proxClicked")
+	},
+	tapClicked: function(){
+		return Session.get("tapClicked")
+	},
+	dragClicked: function(){
+		return Session.get("dragClicked")
+	},
+	flickClicked: function(){
+		return Session.get("flickClicked")
+	},
+	tiltClicked: function(){
+		return Session.get("tiltClicked")
+	},
+	shakeClicked: function(){
+		return Session.get("shakeClicked")
+	},
+	panClicked: function(){
+		return Session.get("panClicked")
+	},
+	waveClicked: function(){
+		return Session.get("waveClicked")
+	},
+	armsClicked: function(){
+		return Session.get("armsClicked")
+	},
+	handClicked: function(){
+		return Session.get("handClicked")
+	},
+	handsClicked: function(){
+		return Session.get("handsClicked")
+	},
+	fingerClicked: function(){
+		return Session.get("fingerClicked")
+	},
+	visionClicked: function(){
+		return Session.get("visionClicked")
+	},
+	hearingClicked: function(){
+		return Session.get("hearingClicked")
+	},
+	speakingClicked: function(){
+		return Session.get("speakingClicked")
+	},
+	standingClicked: function(){
+		return Session.get("standingClicked")
+	},
+	sittingClicked: function(){
+		return Session.get("sittingClicked")
+	},
+	lyingClicked: function(){
+		return Session.get("lyingClicked")
+	},
+	horClicked: function(){
+		return Session.get("horClicked")
+	},
+	vertClicked: function(){
+		return Session.get("vertClicked")
 	}
 });
 //Events
@@ -160,10 +225,10 @@ Template.appDetails.events({
 	
     "keyup #editName": function (){
 		var editedName = $("#editName").val();
-		var appID = this._id;
-		console.log(editedName);
-		console.log(appID);
+		var appId = this._id;
+		console.log(editedName, " ", appId);
 		AppCollection.update({_id: this._id}, {$set: {title: editedName}});
+		return Session.set("editID", false)
 	},
 	"keyup #editDeveloper": function (){
 		var editedDeveloper = $("#editDeveloper").val();
@@ -191,17 +256,285 @@ Template.appDetails.events({
 		AppCollection.update({_id: this._id}, {$set: {price: editedPrice}});
 	},
 	"click #motionIcon": function (){
-		console.log("Clicked");
 		if(Session.get("motionClicked")){
 			console.log("false")
-			AppCollection.update({_id: this._id}, {$set: {motionSensor: false}});
+			AppCollection.update({_id: this._id}, {$set: {"motionSensors": false}});
 			return Session.set("motionClicked", false)
 		}
 		else{
 			console.log("true")
-			AppCollection.update({_id: this._id}, {$set: {motionSensor: true}});
+			AppCollection.update({_id: this._id}, {$set: {"motionSensors": true}});
 			return Session.set("motionClicked", true)
 		}
+	},
+	"click #touchIcon": function (){
+		if(Session.get("touchClicked")){
+			console.log("false")
+			AppCollection.update({_id: this._id}, {$set: {"touchscreen": false}});
+			return Session.set("touchClicked", false)
+		}
+		else{
+			console.log("true")
+			AppCollection.update({_id: this._id}, {$set: {"touchscreen": true}});
+			return Session.set("touchClicked", true)
+		}
+	},
+	"click #micIcon": function (){
+		if(Session.get("micClicked")){
+			console.log("false")
+			AppCollection.update({_id: this._id}, {$set: {"microphone": false}});
+			return Session.set("micClicked", false)
+		}
+		else{
+			console.log("true")
+			AppCollection.update({_id: this._id}, {$set: {"microphone": true}});
+			return Session.set("micClicked", true)
+		}
+	},
+	"click #proxIcon": function (){
+		if(Session.get("proxClicked")){
+			console.log("false")
+			AppCollection.update({_id: this._id}, {$set: {"proximitySensor": false}});
+			return Session.set("proxClicked", false)
+		}
+		else{
+			console.log("true")
+			AppCollection.update({_id: this._id}, {$set: {"proximitySensor": true}});
+			return Session.set("proxClicked", true)
+		}
+	},
+	"click #tapIcon": function (){
+		if(Session.get("tapClicked")){
+			console.log("false")
+			AppCollection.update({_id: this._id}, {$set: {"tap": false}});
+			return Session.set("tapClicked", false)
+		}
+		else{
+			console.log("true")
+			AppCollection.update({_id: this._id}, {$set: {"tap": true}});
+			return Session.set("tapClicked", true)
+		}
+	},
+	"click #dragIcon": function (){
+		if(Session.get("dragClicked")){
+			console.log("false")
+			AppCollection.update({_id: this._id}, {$set: {"drag": false}});
+			return Session.set("dragClicked", false)
+		}
+		else{
+			console.log("true")
+			AppCollection.update({_id: this._id}, {$set: {"drag": true}});
+			return Session.set("dragClicked", true)
+		}
+	},
+	"click #flickIcon": function (){
+		if(Session.get("flickClicked")){
+			console.log("false")
+			AppCollection.update({_id: this._id}, {$set: {"flick": false}});
+			return Session.set("flickClicked", false)
+		}
+		else{
+			console.log("true")
+			AppCollection.update({_id: this._id}, {$set: {"flick": true}});
+			return Session.set("flickClicked", true)
+		}
+	},
+	"click #tiltIcon": function (){
+		if(Session.get("tiltClicked")){
+			console.log("false")
+			AppCollection.update({_id: this._id}, {$set: {"tilt": false}});
+			return Session.set("tiltClicked", false)
+		}
+		else{
+			console.log("true")
+			AppCollection.update({_id: this._id}, {$set: {"tilt": true}});
+			return Session.set("tiltClicked", true)
+		}
+	},
+	"click #shakeIcon": function (){
+		if(Session.get("shakeClicked")){
+			console.log("false")
+			AppCollection.update({_id: this._id}, {$set: {"shake": false}});
+			return Session.set("shakeClicked", false)
+		}
+		else{
+			console.log("true")
+			AppCollection.update({_id: this._id}, {$set: {"shake": true}});
+			return Session.set("shakeClicked", true)
+		}
+	},
+	"click #panIcon": function (){
+		if(Session.get("panClicked")){
+			console.log("false")
+			AppCollection.update({_id: this._id}, {$set: {"pan": false}});
+			return Session.set("panClicked", false)
+		}
+		else{
+			console.log("true")
+			AppCollection.update({_id: this._id}, {$set: {"pan": true}});
+			return Session.set("panClicked", true)
+		}
+	},
+	"click #waveIcon": function (){
+		if(Session.get("waveClicked")){
+			console.log("false")
+			AppCollection.update({_id: this._id}, {$set: {"wave": false}});
+			return Session.set("waveClicked", false)
+		}
+		else{
+			console.log("true")
+			AppCollection.update({_id: this._id}, {$set: {"wave": true}});
+			return Session.set("waveClicked", true)
+		}
+	},
+	"click #armsIcon": function (){
+		if(Session.get("armsClicked")){
+			console.log("false")
+			AppCollection.update({_id: this._id}, {$set: {"arms": false}});
+			return Session.set("armsClicked", false)
+		}
+		else{
+			console.log("true")
+			AppCollection.update({_id: this._id}, {$set: {"arms": true}});
+			return Session.set("armsClicked", true)
+		}
+	},
+	"click #handIcon": function (){
+		if(Session.get("handClicked")){
+			console.log("false")
+			AppCollection.update({_id: this._id}, {$set: {"oneHand": false}});
+			return Session.set("handClicked", false)
+		}
+		else{
+			console.log("true")
+			AppCollection.update({_id: this._id}, {$set: {"oneHand": true}});
+			return Session.set("handClicked", true)
+		}
+	},
+	"click #handsIcon": function (){
+		if(Session.get("handsClicked")){
+			console.log("false")
+			AppCollection.update({_id: this._id}, {$set: {"twoHands": false}});
+			return Session.set("handsClicked", false)
+		}
+		else{
+			console.log("true")
+			AppCollection.update({_id: this._id}, {$set: {"twoHands": true}});
+			return Session.set("handsClicked", true)
+		}
+	},
+	"click #fingerIcon": function (){
+		if(Session.get("fingerClicked")){
+			console.log("false")
+			AppCollection.update({_id: this._id}, {$set: {"finger": false}});
+			return Session.set("fingerClicked", false)
+		}
+		else{
+			console.log("true")
+			AppCollection.update({_id: this._id}, {$set: {"finger": true}});
+			return Session.set("fingerClicked", true)
+		}
+	},
+	"click #visionIcon": function (){
+		if(Session.get("visionClicked")){
+			console.log("false")
+			AppCollection.update({_id: this._id}, {$set: {"vision": false}});
+			return Session.set("visionClicked", false)
+		}
+		else{
+			console.log("true")
+			AppCollection.update({_id: this._id}, {$set: {"vision": true}});
+			return Session.set("visionClicked", true)
+		}
+	},
+	"click #hearingIcon": function (){
+		if(Session.get("hearingClicked")){
+			console.log("false")
+			AppCollection.update({_id: this._id}, {$set: {"hearing": false}});
+			return Session.set("hearingClicked", false)
+		}
+		else{
+			console.log("true")
+			AppCollection.update({_id: this._id}, {$set: {"hearing": true}});
+			return Session.set("hearingClicked", true)
+		}
+	},
+	"click #speakingIcon": function (){
+		if(Session.get("speakingClicked")){
+			console.log("false")
+			AppCollection.update({_id: this._id}, {$set: {"speaking": false}});
+			return Session.set("speakingClicked", false)
+		}
+		else{
+			console.log("true")
+			AppCollection.update({_id: this._id}, {$set: {"speaking": true}});
+			return Session.set("speakingClicked", true)
+		}
+	},
+	"click #standingIcon": function (){
+		if(Session.get("standingClicked")){
+			console.log("false")
+			AppCollection.update({_id: this._id}, {$set: {"standing": false}});
+			return Session.set("standingClicked", false)
+		}
+		else{
+			console.log("true")
+			AppCollection.update({_id: this._id}, {$set: {"standing": true}});
+			return Session.set("standingClicked", true)
+		}
+	},
+	"click #sittingIcon": function (){
+		if(Session.get("sittingClicked")){
+			console.log("false")
+			AppCollection.update({_id: this._id}, {$set: {"sitting": false}});
+			return Session.set("sittingClicked", false)
+		}
+		else{
+			console.log("true")
+			AppCollection.update({_id: this._id}, {$set: {"sitting": true}});
+			return Session.set("sittingClicked", true)
+		}
+	},
+	"click #lyingIcon": function (){
+		if(Session.get("lyingClicked")){
+			console.log("false")
+			AppCollection.update({_id: this._id}, {$set: {"lying": false}});
+			return Session.set("lyingClicked", false)
+		}
+		else{
+			console.log("true")
+			AppCollection.update({_id: this._id}, {$set: {"lying": true}});
+			return Session.set("lyingClicked", true)
+		}
+	},
+	"click #horIcon": function (){
+		if(Session.get("horClicked")){
+			console.log("false")
+			AppCollection.update({_id: this._id}, {$set: {"hor": false}});
+			return Session.set("horClicked", false)
+		}
+		else{
+			console.log("true")
+			AppCollection.update({_id: this._id}, {$set: {"hor": true}});
+			return Session.set("horClicked", true)
+		}
+	},
+	"click #vertIcon": function (){
+		if(Session.get("vertClicked")){
+			console.log("false")
+			AppCollection.update({_id: this._id}, {$set: {"vert": false}});
+			return Session.set("vertClicked", false)
+		}
+		else{
+			console.log("true")
+			AppCollection.update({_id: this._id}, {$set: {"vert": true}});
+			return Session.set("vertClicked", true)
+		}
+	},
+	"keyup #editLink": function (){
+		var editedLink = $("#editLink").val();
+		console.log(editedLink);
+		AppCollection.update({_id: this._id}, {$set: {link: editedLink}});
 	}
 });
 
@@ -214,7 +547,7 @@ Template.appHeader.helpers({
 //Events
 Template.appHeader.events({
     "click #appAdd": function(){
-		AppCollection.insert({title: "Name your app", developer: "Choose a developer"});
+		AppCollection.insert({title: "Name your app", developer: "Choose a developer", rating: 1, category: "Choose a category", video: "Link to a video", price: "", motionSensor: false, touchscreen: false, microphone: false, proximitySensor: false, tap: false, drag: false, flick: false, tilt: false, shake: false, pan: false, wave: false, arms: false, oneHand: false, twoHands: false, finger: false, vision: false, hearing: false, speaking: false, standing: false, sitting: false, lying: false, horizontal: false, vertical: false});
 	}
     
 });
@@ -506,6 +839,10 @@ Template.iconFiltering.events({
 		}
 	}  
 });
+
+
+
+
 
 
 
