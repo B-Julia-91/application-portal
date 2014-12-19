7b4a039816f1ce1c44601f14d36c1fcb9a85efbf
KI-Cortana <nici-jb91@web.de>
2014-12-20 00:50:05 +0100

    Update applicationApi.js

--- server/applicationApi.js
+++ server/applicationApi.js
@@ -5,28 +5,34 @@ Meteor.methods({
 	
 		//PLAY STORE API
 	
-		var apiKey = 'x';	//individual API key
-		var app = "test"//itemID;									//for APP INFO API: package ID of the requested application, which is the same like the itemID of the results of the SEARCH API
+		var apiKey = 'x';	//individual API key									
 		var appCategory = 'apps';								//for now we only use the category "apps", but there are the following categories for SEARCH API: "apps", "books", "movies" or "music"
-		//var keyword = searchfield;						//for SEARCH API
 
-		var urlAppInfo = 'http://api.playstoreapi.com/v1.1/apps/' + app + '?key=' + apiKey;
 		var urlSearch = 'http://api.playstoreapi.com/v1.1/search/' + appCategory + '/' + keyword + '?key=' + apiKey;
-		/*
-		HTTP.get(urlSearch, {params: {}}, function(error,appDetails){
+		console.log("Es wird gesucht");
+		
+		/*HTTP.get(urlSearch, {params: {}}, function(error,appDetails){
 			if(!error){
 				console.log(appDetails.data);
-				for(var i=0; i<appDetails.data.length; i++){
+				for(var i=0; i<1; i++){
 					console.log(appDetails.data[i].itemID);
 					var urlAppInfo = 'http://api.playstoreapi.com/v1.1/apps/' + appDetails.data[i].itemID + '?key=' + apiKey;
 					HTTP.get(urlAppInfo, {params: {}}, function(error,appInfo){
 						if(!error){
-							console.log(appInfo.data[i].appName, appInfo.data[i].developer, appInfo.data[i].category, appInfo.data[i].logo, appInfo.data[i].price, appInfo.data[i].playStoreUrl, appInfo.data[i].video, appInfo.data[i].score);
-						}
+							console.log(appInfo.data);
+							console.log(appInfo.data.appName);
+							console.log(appInfo.data.developer);
+							console.log(appInfo.data.category);
+							console.log(appInfo.data.logo);
+							console.log(appInfo.data.price);
+							console.log(appInfo.data.playStoreUrl);
+							console.log(appInfo.data.score);
+							
+							AppCollection.insert({title: appInfo.data.appName, developer: appInfo.data.developer, rating: appInfo.data.score, category: appInfo.data.category, price: appInfo.data.price, link: appInfo.data.playStoreUrl});
+						}	
 					});
 				}
 			}
-		});
-		*/
+		});*/
 	}
 });
